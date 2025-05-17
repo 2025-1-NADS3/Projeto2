9531c79779a95dea949aa88dd7c9214f6d2fedc3
Felipe <95989152+lipeoe@users.noreply.github.com>
2025-05-17 11:30:18 -0300

    n

--- src/Entrega 3/Backend/server.js
+++ src/Entrega 3/Backend/server.js
@@ -4,14 +4,16 @@ const cors = require('cors')
 const app = express()
 const userRoutes = require('./routes/userRoutes')
 const balanceRoutes = require('./routes/balanceRoutes')
-const boletoRoutes = require('./routes/BoletosRoutes')
+const boletoRoutes = require('./routes/boletosRoutes')
+const produtosRoutes = require('./routes/produtosRoutes')
+
 
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', userRoutes, balanceRoutes, boletoRoutes)
+app.use('/api', userRoutes, balanceRoutes, boletoRoutes, produtosRoutes)
 
 
 app.listen(PORT, () =>{
