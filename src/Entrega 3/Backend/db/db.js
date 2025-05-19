99374e1f8327270d41b6eb6c2d9a336b1c345a17
Felipe <95989152+lipeoe@users.noreply.github.com>
2025-05-18 23:44:39 -0300

    subindo o backend

--- src/Entrega 3/Backend/db/db.js
+++ src/Entrega 3/Backend/db/db.js
@@ -1,13 +1,41 @@
 require("dotenv").config();
+const { Pool } = require("pg");
+const fs = require("fs");
 
-const { Pool } = require('pg')
+let config
 
-const fecapayDB = new Pool ({
+if (process.env.DATABASE_URL) {
+    console.log("Conectado Localmente")
+    config = {
         connectionString: process.env.DATABASE_URL,
-});
+        ssl: process.env.NODE_ENV === "production"
+            ? {
+                  rejectUnauthorized: false,
+                  ca: process.env.SSL_CERT ? fs.readFileSync(process.env.SSL_CERT) : undefined
+              }
+            : false
+    }
+} else {
+    console.log("Conectado na azure")
+    config = {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT || 5432,
+        ssl: process.env.NODE_ENV === "production"
+            ? {
+                  rejectUnauthorized: false,
+                  ca: process.env.SSL_CERT ? fs.readFileSync(process.env.SSL_CERT) : undefined
+              }
+            : false
+    }
+}
+
+const fecapayDB = new Pool(config)
 
 fecapayDB.connect()
-    .then(() => console.log("Conectado ao DB"))
-    .catch(error => console.error("Não conectado", error))
+    .then(() => console.log("✅ Conectado ao banco de dados!"))
+    .catch(error => console.error("❌ Erro ao conectar:", error))
 
-module.exports = fecapayDB    
+module.exports = fecapayDB
--- src/Entrega 3/Backend/server.js
+++ src/Entrega 3/Backend/server.js
@@ -6,6 +6,9 @@ const userRoutes = require('./routes/userRoutes')
 const balanceRoutes = require('./routes/balanceRoutes')
 const boletoRoutes = require('./routes/boletosRoutes')
 const produtosRoutes = require('./routes/produtosRoutes')
+const comprasRoutes = require('./routes/comprasRoutes')
+const transactionsRoutes = require('./routes/transactionRoutes')
+const financasRoutes = require('./routes/financasRoutes')
 
 
 const PORT = process.env.PORT || 3001
@@ -13,7 +16,7 @@ const PORT = process.env.PORT || 3001
 app.use(express.json())
 app.use(cors())
 
-app.use('/api', userRoutes, balanceRoutes, boletoRoutes, produtosRoutes)
+app.use('/api', userRoutes, balanceRoutes, boletoRoutes, produtosRoutes, comprasRoutes, transactionsRoutes, financasRoutes)
 
 
 app.listen(PORT, () =>{
